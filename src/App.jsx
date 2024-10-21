c6ea29d16aba653022720045ab83d2e9abf2234a
rajdevweb <wordhello160@gmail.com>
2024-10-21 14:50:36 +0530

    first-comment

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import './App.css'      
+// import './index.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+{useState}
+const App = () =>{
+  const [num, setnum] = useState(0);
+
+  return(
+    <>
+     <h1>Your number is : {num} </h1>
+
+     <div id='mainBtn'>
+     <button onClick={()=> setnum(num + 10)}>Increment</button>
+     <button onClick={()=> setnum(num - 10)}>Decrement</button>
+     </div>
+
+ 
+    </>
+  )
+}
+
+export default App
+
